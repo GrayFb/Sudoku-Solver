531de6109b7cd618fbc47a58092568c84c0917b8
GrayFb <34057882+GrayFb@users.noreply.github.com>
2018-02-22 09:14:02 +0100

    Update main.py
    3x3 Sudoku

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Datei:    Sudokusolver.py
 # Version:  1.1
-# Datum:    15.02.2018
-# Autor:    Joules and Ikeepgoing
+# Datum:    22.02.2018
+# Autor:    Joules
 
 ##4 Überprüfungen:
 ## 1. Ist Feld=0?
@@ -9,11 +9,22 @@
 ## 3. Gibts sie in der Zeile?
 ## 4. Gibts sie im Block
 
-liste =[[ 2, 4, 0, 0, 9, 0, 0, 0, 0 ],[ 5, 1, 0, 0, 0, 3, 4, 0, 9 ],[ 0, 0, 0, 0, 6, 4, 2, 8, 1 ],[ 0, 0, 0, 3, 0, 8, 1, 0, 0 ],[ 1, 0, 7, 0, 0, 0, 0, 5, 3 ],[ 0, 3, 5, 6, 0, 0, 0, 0, 4 ],[ 0, 5, 1, 7, 0, 0, 0, 0, 2 ],[ 0, 0, 3, 0, 2, 0, 0, 0, 0 ],[ 0, 2, 4, 0, 1, 9, 3, 0, 0 ]];
+'''liste =[[ 2, 4, 0, 0, 9, 0, 0, 0, 0 ],
+        [ 5, 1, 0, 0, 0, 3, 4, 0, 9 ],
+        [ 0, 0, 0, 0, 6, 4, 2, 8, 1 ],
+        [ 0, 0, 0, 3, 0, 8, 1, 0, 0 ],
+        [ 1, 0, 7, 0, 0, 0, 0, 5, 3 ],
+        [ 0, 3, 5, 6, 0, 0, 0, 0, 4 ],
+        [ 0, 5, 1, 7, 0, 0, 0, 0, 2 ],
+        [ 0, 0, 3, 0, 2, 0, 0, 0, 0 ],
+        [ 0, 2, 4, 0, 1, 9, 3, 0, 0 ]];'''
+liste = [[0,0,3],
+         [2,0,0],
+         [0,0,0]];
 
 
 def bedroht(liste, zahl, aktPos, zeile):
-    print "bedroht"
+    #print "bedroht"
     if liste==[]:
         return 0
     
@@ -38,10 +49,10 @@ def bedroht(liste, zahl, aktPos, zeile):
 def loese(aktPos, zeile, liste):
     gesetzt=0
     zahl=1
-    while gesetzt == 0 or zahl<=9:
-        if liste[zeile][aktPos]==0:
-            while zahl<=9 and gesetzt==0:
-                if bedroht(liste, zahl, aktPos, zeile):
+    while zahl<=9 and not gesetzt:
+        if aktPos is not 8 and liste[zeile][aktPos]==0:
+            while zahl<=9 and not gesetzt:
+                if bedroht(liste, zahl, aktPos, zeile) is 1:
                     zahl=zahl+1
                 else:
                     print zeile, aktPos, liste[zeile][aktPos], zahl
@@ -57,12 +68,13 @@ def loese(aktPos, zeile, liste):
             
         if aktPos is 8 and zeile is 8 and gesetzt==1:
             print liste
-        elif aktPos is 8 and gesetzt==1:
-            loese(0, zeile+1, liste )
-        elif gesetzt==1:
+        elif aktPos is 8 and zeile is not 8 and gesetzt==1:
+            loese(0, zeile+1, liste)
+        elif aktPos is not 8 and zeile is not 8 and gesetzt==1:
             loese(aktPos+1, zeile, liste)
         else:
             return 0
+    
                    
 def sudokusolver():
     loese(0,0, liste)
